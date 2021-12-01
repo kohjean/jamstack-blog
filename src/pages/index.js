ce1b89999d912aac233f93dc83de76be4bc8f754
kohjean <58461459+kohjean@users.noreply.github.com>
2021-12-01 14:24:26 +0900

    Change to display images with fixed width

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,7 @@ export default function Home({ data }) {
           <div className="details">
             <div className="detail">
               <figure>
-                <Img fluid={data.fruit.childImageSharp.fluid} alt="" />
+                <Img fixed={data.fruit.childImageSharp.fixed} alt="" />
               </figure>
               <h3>フルーツ</h3>
               <p>FRUIT</p>
@@ -84,7 +84,7 @@ export default function Home({ data }) {
             </div>
             <div className="detail">
               <figure>
-                <Img fluid={data.grain.childImageSharp.fluid} alt="" />
+                <Img fixed={data.grain.childImageSharp.fixed} alt="" />
               </figure>
               <h3>穀物</h3>
               <p>GRAIN</p>
@@ -96,7 +96,7 @@ export default function Home({ data }) {
             </div>
             <div className="detail">
               <figure>
-                <Img fluid={data.beverage.childImageSharp.fluid} alt="" />
+                <Img fixed={data.beverage.childImageSharp.fixed} alt="" />
               </figure>
               <h3>飲み物</h3>
               <p>BEVERAGE</p>
@@ -183,22 +183,22 @@ export const query = graphql`
     }
     fruit: file(relativePath: { eq: "fruit.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 320) {
-          ...GatsbyImageSharpFluid_withWebp
+        fixed(width: 200) {
+          ...GatsbyImageSharpFixed_withWebp
         }
       }
     }
     grain: file(relativePath: { eq: "grain.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 320) {
-          ...GatsbyImageSharpFluid_withWebp
+        fixed(width: 200) {
+          ...GatsbyImageSharpFixed_withWebp
         }
       }
     }
     beverage: file(relativePath: { eq: "beverage.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 320) {
-          ...GatsbyImageSharpFluid_withWebp
+        fixed(width: 200) {
+          ...GatsbyImageSharpFixed_withWebp
         }
       }
     }
