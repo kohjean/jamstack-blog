a00e570a63326973990ea77a8b8d70b1c6258720
kohjean <58461459+kohjean@users.noreply.github.com>
2021-12-01 21:32:11 +0900

    Add meta-data for OGP image

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,6 +16,9 @@ export default function Home({ data, location }) {
         pagetitle="ESSENTIALSについて"
         pagedesc="食べ物についての情報を発信しているサイトです"
         pagepath={location.pathname}
+        pageimg={data.about.childImageSharp.original.src}
+        pageimgw={data.about.childImageSharp.original.width}
+        pageimgh={data.about.childImageSharp.original.height}
       />
       <div className="eyecatch">
         <figure>
@@ -73,6 +76,11 @@ export const query = graphql`
       childImageSharp {
         fluid(maxWidth: 1600) {
           ...GatsbyImageSharpFluid_withWebp
+        }
+        original {
+          src
+          height
+          width
         }
       }
     }
