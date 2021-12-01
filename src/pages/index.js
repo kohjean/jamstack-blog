0a808451c9e73f6e4bec4b3fd62dfced2c1955d4
kohjean <58461459+kohjean@users.noreply.github.com>
2021-12-01 13:52:31 +0900

    Change footer image to gatsby-image

--- src/pages/index.js
+++ src/pages/index.js
@@ -160,6 +160,13 @@ export default function Home({ data }) {
             </li>
           </ul>
         </div>
+        <div className="back">
+          <Img
+            fluid={data.pattern.childImageSharp.fluid}
+            alt=""
+            style={{ height: "100%" }}
+          />
+        </div>
       </footer>
     </>
   )
@@ -198,6 +205,13 @@ export const query = graphql`
     berry: file(relativePath: { eq: "berry.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    pattern: file(relativePath: { eq: "pattern.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1920) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
