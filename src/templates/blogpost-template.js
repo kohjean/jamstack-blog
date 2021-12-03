ff54284dadf938ec06b1d8ee29c0855f7f880664
kohjean <58461459+kohjean@users.noreply.github.com>
2021-12-03 21:32:32 +0900

    Add link to category on each page

--- src/templates/blogpost-template.js
+++ src/templates/blogpost-template.js
@@ -44,7 +44,7 @@ export default function BlogPost({ data, pageContext, location }) {
               <ul>
                 {data.microcmsBlog.category.map(cat => (
                   <li className={cat.categorySlug} key={cat.id}>
-                    {cat.category}
+                    <Link to={`/cat/${cat.categorySlug}/`}>{cat.category}</Link>
                   </li>
                 ))}
               </ul>
